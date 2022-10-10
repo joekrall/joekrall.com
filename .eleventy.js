d4f0ed19db2719a05695e7b3ddc9256d55e40c2d
Joe Krall <joe(at)joekrall.com>
2022-10-09 19:46:22 -0700

    now, let's delete the timezone

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
   eleventyConfig.addFilter("isoDate", (dateObj) => {
     return DateTime
-      .fromJSDate(dateObj, { zone: 'America/Los_Angeles' })
+      .fromJSDate(dateObj)
       .toUTC()
       .toISO({ includeOffset: true });
   });
