674ece092e14fd2f27a9ee083774602046851e0f
Joe Krall <63315344+joekrall@users.noreply.github.com>
2023-03-27 16:00:35 -0700

    more spring cleaning ☀

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,12 @@
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+const rss = require("@11ty/eleventy-plugin-rss");
+const footnote = require("markdown-it-footnote");
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(rss);
   eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
   eleventyConfig.addFilter("postDate", (dateObj) => {
-    return DateTime.fromJSDate(dateObj).toUTC().toFormat('LLLL dd, yyyy');
+    return DateTime.fromJSDate(dateObj).toUTC().toFormat("LLLL dd, yyyy");
   });
   eleventyConfig.addFilter("noRssOnlyPosts", (posts) => {
     return posts.filter((post) => !post.data.tags.includes("RSS Only"));
@@ -13,14 +14,15 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addGlobalData("generated", () => {
     return DateTime.now().toUTC();
   });
+  eleventyConfig.amendLibrary("md", mdLib => mdLib.use(footnote));
 
   return {
       dir: { 
-        input: 'src',
+        input: "src",
         data: "_data",
         includes: "_includes",
         layouts: "_layouts",
-        output: '_site' 
+        output: "_site",
       }
   }; 
 };
